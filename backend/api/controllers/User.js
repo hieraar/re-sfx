87e7fb4897ec7ff6bce2e308c745a2241f2d8490
hieraar <hiera.ahm2002@mail.ugm.ac.id>
2024-01-25 12:39:18 +0700

    Menambahkan sign in, get user data, dan update user

--- backend/api/controllers/User.js
+++ backend/api/controllers/User.js
@@ -43,33 +43,78 @@ exports.userSignUp = async (req, res) => {
     }
   };
 
-exports.userSignIn = async (req, res) => {
-  const { email, password } = req.body;
+  exports.userSignIn = async (req, res) => {
+    const { identifier, password } = req.body; 
+    try {
+      // Check if the identifier is an email
+      const user = await User.findOne({ $or: [{ email: identifier }, { username: identifier }] });
+  
+      if (!user) {
+        return res.status(401).json({ message: "Invalid email or username, please try again" });
+      }
+  
+      const passwordMatch = await bcrypt.compare(password, user.password);
+  
+      if (!passwordMatch) {
+        return res.status(401).json({ message: "Invalid password, please try again" });
+      }
+  
+      const token = jwt.sign({ _id: user.id }, process.env.JWT_SECRET, {
+        expiresIn: "3h",
+      });
+  
+      user.password = undefined;
+  
+      res
+        .status(200)
+        .json({ message: "Sign-in successful", user, token, role: "user" });
+  
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Server error" });
+    }
+  };
+
+exports.getUserById = async (req, res) => {
+  const userId = req.params.id;
   try {
-    const user = await User.findOne({ email });
-
+    const user = await User.findById(userId);
     if (!user) {
-      return res.status(401).json({ message: "Invalid email or password" });
+      return res.status(404).json({ message: "User not found" });
     }
-
-    const passwordMatch = await bcrypt.compare(password, user.password);
-
-    if (!passwordMatch) {
-      return res.status(401).json({ message: "Invalid email or password" });
-    }
-
-    const token = jwt.sign({ _id: user.id }, process.env.JWT_SECRET, {
-      expiresIn: "3h",
+    res.status(200).json({
+      name: user.name,
+      username: user.username,
+      email: user.email,
+      soundCount: user.soundCount,
+      message: "Customer data retrieved succesfully",
     });
-
-    user.password = undefined;
-
-    res
-      .status(200)
-      .json({ message: "Sign-in successful", user, token, role: "user" });
-
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
   }
 };
+
+exports.updateUserData = async (req, res) =>{
+  const userId = req.params.id;
+  const { newName, newUserName, newEmail} = req.body; 
+  try {
+    const user = await User.findById(userId);
+    if(!user){
+      return res.status(404).json({ message: "User not found"});
+    }
+    
+    user.name= newName;
+    user.username= newUserName;
+    user.email=newEmail;
+
+    await user.save(); 
+    res.status(200).json({
+      message: "User data updated successfully",
+    });
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+}
--- backend/api/routes/User.js
+++ backend/api/routes/User.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const { userSignUp } = require("../controllers/User");
+const { userSignIn } = require("../controllers/User");
+const { getUserById } = require("../controllers/User");
+const { updateUserData } = require("../controllers/User");
 const router = express.Router();
 
-router.post("/signin", userSignUp);
+router.post("/signup", userSignUp);
+router.post("/signin", userSignIn);
+router.get("/:id", getUserById);
+router.put("/:id/updatedata", updateUserData);
 
 module.exports = router;
