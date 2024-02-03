584e9e4a80dbac57f7af630595748718d814406c
hieraar <hiera.ahm2002@mail.ugm.ac.id>
2024-02-03 13:28:21 +0700

    coba lagi jwt

--- backend/api/routes/Sounds.js
+++ backend/api/routes/Sounds.js
@@ -8,11 +8,12 @@ const { playSoundById } = require("../controllers/Sounds");
 const { getAllSounds } = require("../controllers/Sounds");
 const multer = require('multer');
 const router = express.Router();
+const { authenticateToken } = require('..//middleware/authToken');
 
 const storage = multer.memoryStorage(); 
 const upload = multer({ storage: storage });
 
-router.post('/upload', upload.single('file'), uploadSound);
+router.post('/upload', upload.single('file'), uploadSound, authenticateToken);
 router.delete('/delete/:soundId', deleteSound);
 router.get('/getonesound/:soundId', getSoundById);
 router.get('/getsounds', getSoundsByOwner);
